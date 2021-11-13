e829b4fbc68a76a2105622f3c3261badf15b70c6
Moses <mosesobike@gmail.com>
2021-11-13 06:24:40 +0100

    add card section

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../style.css"
+
+export default function Navabr(){
+        return(
+            <nav>
+                <img src= "/assets/images/airbnb.png" alt ="logo" />
+            </nav>
+        )
+}
+
